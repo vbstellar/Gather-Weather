e1fc050c417d89c08c4e866d225babafcc0e3062
vishu <bansalvishu56@gmail.com>
2023-01-05 22:05:58 +0530

    Data Displayed on Frontend

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const app = express();
 
 const https = require("https")
 
-const url = "https://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=6b11f61fb89f3046f564e2d89c422261";
+const url = "https://api.openweathermap.org/geo/1.0/direct?q=delhi&limit=1&appid=6b11f61fb89f3046f564e2d89c422261";
 var url2 =""
+var temp = "";
+var description = "";
 
 
 app.get("/", function(req, res){
@@ -15,8 +17,8 @@ app.get("/", function(req, res){
 
         response.on("data", function(data){
             const degreeData = JSON.parse(data);
-            const lat = (parseFloat(degreeData[0].lat).toFixed(2)).toString();
-            const lon = parseFloat(degreeData[0].lon).toFixed(2).toString();
+            var lat = (parseFloat(degreeData[0].lat).toFixed(2)).toString();
+            var lon = parseFloat(degreeData[0].lon).toFixed(2).toString();
             console.log(lat +" " +lon);
             
             url2 = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=6b11f61fb89f3046f564e2d89c422261&units=metric";
@@ -29,19 +31,14 @@ app.get("/", function(req, res){
 
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const description = weatherData.weather[0].description;
+            temp = weatherData.main.temp;
+            description = weatherData.weather[0].description;
             console.log(temp + " " + description);
         })
     })
 
-    res.send("The server is up and running.")
+    res.send(`<h1>The temperature in Delhi is: ${temp} °C</h1>`);
 })
-
-
-
-
-
 
 
 
